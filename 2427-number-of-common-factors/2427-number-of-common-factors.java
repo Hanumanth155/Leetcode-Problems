10489f383a29dd937e9e4a80135442dff3f12eac
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-06-27 22:47:07 +0530

    Time: 0 ms (100%), Space: 42 MB (77.22%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,14 +1,26 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int higher = a,count=0;
-        if(b>a){
-            higher=b;
-        }
-        for(int i=1;i<=higher;i++){
-            if(a%i==0&&b%i==0){
+        int count=0;
+        int gcd = findGCD(a,b);
+        for(int i=1;i*i<=gcd;i++){
+            if(gcd%i==0){
                 count++;
+                if(i!=(gcd/i)){
+                    count++;
+                }
             }
         }
         return count;
+    }
+    public static int findGCD(int a,int b){
+        while(a>0){
+            if(a<b){
+                int temp=a;
+                a=b;
+                b=temp;
+            }
+            a=a%b;
+        }
+        return b;
     }
 }

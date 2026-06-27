b0f252148fa430c3ccce4cbf4060d777ab55d20c
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-06-27 21:53:10 +0530

    Time: 5 ms (13.41%), Space: 42.8 MB (5.84%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -13,20 +13,26 @@ class Solution {
         return n2;
         
     }
-    public static int evenSum(int n){
-        int sum=0;
-        while(n>0){
-            sum=sum+2;
-            n--;
+        public static int evenSum(int n){
+            int sum=0,series=2;
+            while(n>0){
+                if(series%2==0){
+                    sum+=series;
+                    n--;
+                }
+                series++;
+            }
+            return sum;
         }
-        return sum;
-    }
-    public static int oddSum(int n){
-        int sum=0;
-        while(n>0){
-            sum=sum+1;
-            n--;
+        public static int oddSum(int n){
+            int sum=0,series=1;
+            while(n>0){
+                if(series%2!=0){
+                    sum+=series;
+                    n--;
+                }
+                series++;
+            }
+            return sum;
         }
-        return sum;
-    }
 }

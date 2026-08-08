fba9449259a169cfa1839fefc3f5f07c5bd40888
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-08-08 20:43:53 +0530

    Time: 1 ms (76.44%), Space: 42.4 MB (51.44%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,29 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {
-        
-        while(n!=1&&n!=4){
-            n=isASN(n);
-        }
-        return n==1;
-    }
-
-    public static int expoenent(int base,int pow){
-        int expo=1;
-        while(pow>0){
-            expo*=base;
-            pow--;
-        }
-        return expo;
-    }
-    
-    public static int isASN(int n){
-        int base,pow,asn=0;
-         pow=2;
-        while(n>0){
-            base=n%10;
-            asn=asn+expoenent(base,pow);
+        HashSet<Integer> s = new HashSet<>();
+        int sum;
+        while(n!=1){
+            sum=0;
+        while(n!=0){
+            int rem = n%10;
+            sum = sum+ (rem*rem);
             n/=10;
         }
-        return asn;
+        if(s.contains(sum)){
+            return false;
+        }
+        s.add(sum);
+        n=sum;
+        }
+        return true;
     }
 }

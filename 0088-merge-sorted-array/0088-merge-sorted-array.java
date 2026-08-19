ec2238cd661c009692bf98c19e43a145ca89cd55
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-08-19 19:41:09 +0530

    Time: 4 ms (17.25%), Space: 44.2 MB (9.17%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0;
+        for(;m<nums1.length;m++){
+            nums1[m]=nums2[i++];
+        }
+        Arrays.sort(nums1);
+    }
+}

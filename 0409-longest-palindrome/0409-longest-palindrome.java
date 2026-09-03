5f44fe130ebf1a350daaa262de241f955db2d6de
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-09-03 23:26:23 +0530

    Time: 2 ms (75.16%), Space: 42.3 MB (99.78%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        int[] count = new int[128];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < count.length; i++) {
+            ans += (count[i] / 2) * 2;
+        }
+
+        if (ans < s.length()) {
+            ans++;
+        }
+
+        return ans;
+    }
+}

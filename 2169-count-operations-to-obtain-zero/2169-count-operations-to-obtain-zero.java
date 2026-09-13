0868b7e3f0737b76c6e6ca3ade64190d751fe1ca
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-09-13 20:58:47 +0530

    Time: 6 ms (5.9%), Space: 47.8 MB (5.04%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,17 @@
+class Solution {
+    int count=0;
+    public int countOperations(int num1, int num2) {
+        
+        if(num1==0 || num2==0){
+            return count;
+        }
+        if(num1<=num2){
+            num2 = num2-num1;
+            count++;
+        }else{
+            num1=num1-num2;
+            count++;
+        }
+        return countOperations(num1,num2);
+    }
+}

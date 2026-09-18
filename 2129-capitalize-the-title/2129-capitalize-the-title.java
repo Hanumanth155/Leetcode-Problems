19257b8e182d1d9cd7c004e821233ea527c2841c
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-09-18 19:15:16 +0530

    Time: 3 ms (76.69%), Space: 43.8 MB (62.66%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String title1 = title.toLowerCase(); 
+        String[] words = title1.split(" ");
+        StringBuilder res = new StringBuilder();
+        StringBuilder sb;
+        for(int i=0;i<words.length;i++){
+            if(words[i].length()<=2){
+              sb = new StringBuilder(words[i]);
+            }else{
+              sb = new StringBuilder(words[i]);
+              sb.setCharAt(0,(char)(sb.charAt(0)-32));; 
+            }  
+            res.append(sb);
+            if(i!=words.length-1){
+                res.append(" ");
+            }     
+        }
+        return res.toString();
+    }
+}

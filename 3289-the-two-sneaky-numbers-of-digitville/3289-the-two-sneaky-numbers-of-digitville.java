a027ea47768ab1bfdf07413c242f3317a04922d1
Hanumanta Devu Gouda <hanumanthgouda93@gmail.com>
2026-09-23 16:28:55 +0530

    Time: 3 ms (32.11%), Space: 46.7 MB (22.35%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,10 +1,14 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         HashMap<Integer,Integer> h = new HashMap<>();
+        int count=0;
         for(int num :nums){
             h.put(num,h.getOrDefault(num,0)+1);
+            if(h.get(num)==2){
+                count++;
+            }
         }
-        int[] res = new int[2];
+        int[] res = new int[count];
         int index=0;
         for(int i : h.keySet()){
             if(h.get(i)==2){
